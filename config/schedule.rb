33b7ffc4dfe18de23b22728e326d4ffe152c26ca
masaya-tn <uvermasayan@icloud.com>
2021-03-25 22:19:20 +0900

    アウトプット、アクションプラン等のデザインを整えた

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,10 @@ env :PATH, ENV['PATH']
 set :output, 'log/cron.log'
 set :environment, :development
 
-# every :sunday, at: '9pm' do
-#   runner 'EmailDeliverJob.perform_later'
-# end
-
-every 1.minutes do
+every :sunday, at: '9pm' do
   runner 'EmailDeliverJob.perform_later'
 end
+
+# every 1.minutes do
+#   runner 'EmailDeliverJob.perform_later'
+# end

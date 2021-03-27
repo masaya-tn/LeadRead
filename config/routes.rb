2e56aea073f5090c4b4cbe52edb5403c0ad4dba7
masaya-tn <uvermasayan@icloud.com>
2021-03-27 17:17:15 +0900

    ゲストログイン機能 #48

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   resources :relationships, only: %i[create destroy]
   resources :books, only: %i[create show], shallow: true do
     collection do

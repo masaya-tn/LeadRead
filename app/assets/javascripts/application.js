61f2c207adc8ce2525863e417faa78bf7b3b7492
masaya-tn <uvermasayan@icloud.com>
2021-04-02 13:32:12 +0900

    aws

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 //= require jquery3
 //= require popper
 //= require rails-ujs
-//= require bootstrap-material-design/dist/js/bootstrap-material-design.js
+//= require bootstrap-material-design/dist/js/bootstrap-material-design
 
